5ac3be0118a7f6f12363e659e32fba19eddd46a8
marcelocardenastrigo <marcelo.oscar.cardenas.trigo@gmail.com>
2020-04-22 00:14:30 -0400

    commit10

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 class Tasks extends Component {
     render() {
         return <div>
-        {this.props.tasks.map(e => <p key={e.id}>
+        {this.props1.tasks.map(e => <p key={e.id}>
             {e.title} - {e.description} - {e.done} - {e.id}
             
             </p>)
